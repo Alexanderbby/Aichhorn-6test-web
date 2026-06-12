ca5cb36da72867da33f37b2b3dd3e48756e17831
Alexanderbby <alexander.aichhorn@khev.at>
2026-06-12 10:15:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+const liste = ["Am Strand entspannen", "Neue Städte entdecken", "Gutes Essen genießen", "Fotos machen"];
+const ul = document.getElementById("urlaubsListe");
+
+liste.forEach(eintrag => {
+    const li = document.createElement("li");
+    li.textContent = eintrag;
+    ul.appendChild(li);
+});
 
